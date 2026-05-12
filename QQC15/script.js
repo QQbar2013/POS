e45f54319f566c1210eaed0a9a04005ff6c611ec
QQbar2013 <jacky999200@gmail.com>
2026-05-12 19:26:22 +0800

    Update script.js

--- QQC15/script.js
+++ QQC15/script.js
@@ -3,38 +3,27 @@ $(document).ready(function(){
   $('#totalQtyInput').on('keyup click change input', function(){
     var unitPrice = 15;
     var totalQty = parseInt($(this).val());
-    var $hintText = $('#hintText');
     
     if (isNaN(totalQty) || totalQty <= 0) {
       resetDisplay();
       return;
     }
     
-    // 您的核心精密公式
-    // 1. 購買 = ROUNDUP(總數 / 1.1, 0)
+    // 公式校驗
     var paidQty = Math.ceil(totalQty / 1.1);
-    
-    // 2. 贈送 = ROUNDDOWN(購買 / 10, 0)
     var freeQty = Math.floor(paidQty / 10);
-    
-    // 3. 應有總數 = 購買 + 贈送
     var logicTotal = paidQty + freeQty;
-    
-    // 4. 計算差距 X
     var diffX = logicTotal - totalQty;
     
     if (diffX !== 0) {
-      // 邏輯不符，顯示「請再選 X 枝」
       $('#resultArea').hide();
-      $hintText.text('💡 請再選 ' + diffX + ' 枝');
+      $('#hintText').text('💡 請再選 ' + diffX + ' 枝');
       $('#hintArea').show();
     } else {
-      // 邏輯相符，顯示結帳資訊
       $('#hintArea').hide();
       $('#resultArea').show();
       
       var finalAmount = paidQty * unitPrice;
-
       $('#displayTotal').text(totalQty);
       $('#promoText').text('買 ' + paidQty + ' 送 ' + freeQty);
       $('#calcPaid').text(paidQty);
